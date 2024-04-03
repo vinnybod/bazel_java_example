fb8720690f010a672fd41649b28ada56108ee9b0
Vincent Rose <vrose@confluent.io>
2024-04-02 17:20:46 -0700

    test junitxml

---
+++ src/main/java/com/example/EntryPointTest.java
@@ -0,0 +1,14 @@
+package main.java.com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EntryPointTest {
+
+    @Test
+    public void testMain() {
+        assertFalse(true);
+    }
+
+}
